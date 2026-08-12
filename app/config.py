2f5c8e3d7731428aa12a657ed5241f294db6b163
MerlKoryAmber <llevelamoney@gmail.com>
2026-08-12 17:04:38 +0300

    Delete app directory

--- app/config.py
+++
@@ -1,7 +0,0 @@
-# app/config.py
-import os
-from pathlib import Path
-SECRET_KEY = os.getenv("SQUIDMGR_SECRET_KEY", "supersecretdefault")
-SQLALCHEMY_DATABASE_URI = "sqlite:///{}/db.sqlite3".format(os.path.dirname(__file__))
-SQUID_CONF_PATH = "/etc/squid/squid.conf"
-BACKUP_DIR = Path("/opt/squid-panel/backups")
