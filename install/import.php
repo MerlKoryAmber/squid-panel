3aa8af71d7cb2c7453ad9956f604e73dd8d7fbbf
MerlKoryAmber <llevelamoney@gmail.com>
2026-08-13 23:53:06 +0300

    Add files via upload

--- install/import.php
+++ install/import.php
@@ -1,5 +1,4 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../config/app.php';
 require_once __DIR__ . '/../config/squid.php';
 require_once __DIR__ . '/../app/Core/Database.php';
